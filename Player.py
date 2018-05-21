e6e0956bd7907a9f9a00ee1da5cb0124e2faac26
guitav <guitapardis@yahoo.com>
2018-05-21 15:14:02 -0700

    Working AI, ai vs ai works; uncompleted array column

--- Player.py
+++ Player.py
@@ -62,9 +62,9 @@ class AIPlayer:
         result += self.count_values( board, 3, player) * 100
         result += self.count_values( board, 2, player) * 10
 
-        result += self.count_values( board, 4, opponent) * 500 
-        result += self.count_values( board, 3, opponent) * 100 
-        result += self.count_values( board, 2, opponent) * 10
+        result -= self.count_values( board, 4, opponent) * 500 
+        result -= self.count_values( board, 3, opponent) * 100 
+        result -= self.count_values( board, 2, opponent) * 10
 
         return (result)
 
@@ -72,11 +72,11 @@ class AIPlayer:
         values = []
     
         def alphabeta( board, depth, alpha, beta, player, opponent):
-            v = -100000
             for row, col in self.validMoves(board):
                 board[row][col] = player
-                v = max(v, min_value(board,alpha, beta,depth, player, opponent))
-                values.append(v)
+                alpha = max(alpha, min_value(board,alpha, beta,depth - 1 , player, opponent))
+                values.append(alpha)
+                print (values)
                 board[row][col] = 0
             maxvalue = max(values)
             maxindex = values.index(maxvalue)
@@ -86,41 +86,35 @@ class AIPlayer:
             valid_moves = self.validMoves(board)
             if(depth == 0 or not valid_moves):
                 return (self.evaluation_function(board))
-            v = +10000000
             for row,col in valid_moves:
                 board[row][col] = opponent 
                 result = max_value(board, alpha, beta, depth-1, player, opponent)
-                print("min board")
-                print(board)
-                v = min (v, result)
+                beta = min (beta, result)
                 board[row][col] = 0
-                if v<= alpha:
-                    return v
-                beta = min(beta,v)
-            return v
+                if beta<= alpha:
+                    return beta 
+            return beta
         def max_value(board,alpha, beta, depth, player, opponent):
             valid_moves = self.validMoves(board)
             if(depth == 0 or not valid_moves):
                 return (self.evaluation_function(board))
-            v = -10000000
             for row, col in valid_moves:
                 board[row][col] = player 
                 print("max board")
                 print(board)
                 result = min_value(board,alpha,beta,depth-1, player, opponent)
-                v = max(v, result)
+                alpha = max(alpha, result)
                 board[row][col] = 0
-                if v >= beta:
-                    return v
-                alpha = max(alpha, v)
-            return v
+                if alpha >= beta:
+                    return alpha
+            return alpha
 
         player = self.player_number
         if (player == 1): 
             opponent = 2
         else: 
             opponent = 1
-        return (alphabeta(board, 2, -100000,+100000, player, opponent)) 
+        return (alphabeta(board, 4, -100000,+100000, player, opponent)) 
         raise NotImplementedError('Whoops I don\'t know what to do')
 #    def get_alpha_beta_move(self, board):
 #        player = self.player_number
